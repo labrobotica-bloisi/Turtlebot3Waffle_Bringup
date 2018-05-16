02cb01fd56daca07a5b1522949579d2f30086884
dbloisi <bloisi@dis.uniroma1.it>
2018-05-16 10:22:16 +0200

    cambi

--- 07turtlebot_realsense.sh
+++ 07turtlebot_realsense.sh
@@ -8,5 +8,7 @@ source ./00config.sh
 
 ./00sendconfiguration.sh
 
-ssh ${JOULE_USERNAME}@${JOULE_IP} "bash -c 'chmod +x joule_config.sh; source ./joule_config.sh; roslaunch turtlebot3_bringup turtlebot3_realsense.launch'"
+#ssh ${JOULE_USERNAME}@${JOULE_IP} "bash -c 'chmod +x joule_config.sh; source ./joule_config.sh; roslaunch turtlebot3_bringup turtlebot3_realsense.launch'"
+
+ssh ${JOULE_USERNAME}@${JOULE_IP} "bash -c 'chmod +x joule_config.sh; source ./joule_config.sh; roslaunch realsense_camera r200_nodelet_default.launch'"
 
